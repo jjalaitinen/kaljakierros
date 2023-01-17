4c57a92fd77c223dbf524d99b899c42fbd18b260
Tuukka Pitkänen <tuukkapitkanen2@gmail.com>
2023-01-17 15:52:04 +0200

    arpomislogiikka lisätty

--- hello.py
+++ hello.py
@@ -1,5 +1,6 @@
 from flask import Flask
 import json
+import random
 
 app = Flask(__name__)
 
@@ -7,8 +8,9 @@ app = Flask(__name__)
 def hello_world():
     baarit_json = open('baarit.json')
     baarit_lista = json.load(baarit_json)
-    
-    for baari in baarit_lista:
+    arvottavien_lkm = 5
+    arvotut_baarit = random.choices(baarit_lista, k=5)
+    for baari in arvotut_baarit:
         print(baari["nimi"])
 
     return "<p>Hello, World!</p>"
