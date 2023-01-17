1fd1e4b4f9e8db33e8b2c05721016ed147d459fa
Tuukka Pitkänen <tuukkapitkanen2@gmail.com>
2023-01-17 15:19:27 +0200

    Testasin muutoksia. Lisätty baarit.json ja sen lukeminen

--- hello.py
+++ hello.py
@@ -1,7 +1,14 @@
 from flask import Flask
+import json
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
+    baarit_json = open('baarit.json')
+    baarit_lista = json.load(baarit_json)
+    
+    for baari in baarit_lista:
+        print(baari["nimi"])
+
     return "<p>Hello, World!</p>"
