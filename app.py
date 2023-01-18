f35d9aa8266d2621f8beeed4d14659b79d67724e
Tuukka Pitkänen <tuukkapitkanen2@gmail.com>
2023-01-18 20:03:28 +0200

    etusivulle tullessa arvotaan 5 baarin nimeä

--- app.py
+++ app.py
@@ -6,18 +6,14 @@ app = Flask(__name__)
 
 @app.route("/")
 def etusivu():
-    return render_template("index.html")
+    baarit_json = open("baarit.json")
+    baarit_lista = json.load(baarit_json)
+    baarit_json.close()
+    arvottavien_lkm = 5
+    arvotut_baarit = random.sample(baarit_lista, k=arvottavien_lkm)
+    return render_template("index.html",baarit=arvotut_baarit)
 
-@app.route("/testi", methods=['GET'])
+@app.route("/testi", methods=["GET"])
 def testi():
     print("toimii!")
-    baarit_json = open('baarit.json')
-    baarit_lista = json.load(baarit_json)
-    arvottavien_lkm = 5
-    arvotut_baarit = random.choices(baarit_lista, k=arvottavien_lkm)
-    #for baari in arvotut_baarit:
-    #    print(baari["nimi"])
-    tulostus = jsonify(arvotut_baarit)
-    print(tulostus)
-
     return tulostus
