277035cd0b1b9423bf68edc8ba36c31c2037348f
tupepi <tuukkapitkanen2@gmail.com>
2023-01-20 21:25:33 +0200

    app.py kaikki baari jsonista

--- app.py
+++ app.py
@@ -9,9 +9,17 @@ def etusivu():
     arvotut_baarit = arvo_baarit(5)
     return render_template("index.html",baarit=arvotut_baarit)
 
+@app.route("/kaikki-baarit", methods=["GET"])
+def kaikki_baarit():
+    baarit_json = open("baarit.json")
+    baarit_lista = json.load(baarit_json)
+    baarit_json.close()
+    return jsonify(baarit_lista)
+
 @app.route("/baarit", methods=["GET"])
 def baarit():
     return jsonify(arvo_baarit(5))
+
 
 def arvo_baarit(lkm):
     baarit_json = open("baarit.json")
