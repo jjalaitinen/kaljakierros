b01029c57f3d2a231d38849ce8a6d3423d220bd1
laitjjxz <joona.j.laitinen@student.jyu.fi>
2023-01-20 21:31:32 +0200

    toiminta.js

--- static/toiminta.js
+++ static/toiminta.js
@@ -1,18 +1,25 @@
-$(document).ready(function () {
-  $('#testi_painike').click(function () {
-    $.ajax({
-      data: {},
-      type: 'GET',
-      url: '/baarit',
-      success: function (data) {
-        baarilista = $('#baarilista');
-        baarilista.empty();
-        data.forEach((baari) => {
-          li = document.createElement('li');
-          li.textContent = baari['nimi'];
-          baarilista.append(li);
-        });
-      },
-    });
+function listaa_baarit() {
+  let baarit;
+}
+
+function lisaa_kasittelijat() {}
+
+function hae_kaikki_baarit() {
+  localStorage.clear();
+  $.ajax({
+    data: {},
+    type: "GET",
+    url: "/kaikki-baarit",
+    success: function (data) {
+      localStorage.setItem("baaritiedot", data);
+    },
   });
+}
+
+$(document).ready(function () {
+  hae_kaikki_baarit();
+  listaa_baarit();
+  lisaa_kasittelijat();
+
+  $("#testi_painike").click(function () {});
 });
