0f7f9d7c1447b3f8027f1ebb7f0eb9a481c0e293
Tuukka Pitkänen <tuukkapitkanen2@gmail.com>
2023-02-08 13:25:01 +0200

    tallennetaan localstorageen milloin viimeksi haettu baaritiedot ja jos on mennyt yli 10 sekuntia haetaan sivulle tultaessa uudestaan

--- static/toiminta/pyynnot.js
+++ static/toiminta/pyynnot.js
@@ -1,10 +1,21 @@
 async function hae_kaikki_baarit() {
+  let tietojen_hakuaika = localStorage.getItem("tietojen_hakuaika");
+  if (tietojen_hakuaika) {
+    let millisekunnit = parseInt(tietojen_hakuaika,0);
+    let erotus = (Date.now() - millisekunnit)/1000;
+    // Jos on kulunut alle 10 sekuntia viimeisestä baari.json hausta, ei haeta uudelleen
+    if (erotus < 10) {
+      return;
+    }
+  }
+  
   localStorage.clear();
   await $.ajax({
     data: {},
     type: "GET",
     url: "/kaikki-baarit",
     complete: function (data) {
+      localStorage.setItem("tietojen_hakuaika", Date.now());
       localStorage.setItem("baaritiedot", JSON.stringify(data.responseJSON));
     },
   });
