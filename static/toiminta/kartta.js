b5ed48d9ccf12ce803a986c305bcb4974d4a50a9
tupepi <tuukkapitkanen2@gmail.com>
2023-02-18 17:02:27 +0200

    kartan zoomauksen hienosäätö

--- static/toiminta/kartta.js
+++ static/toiminta/kartta.js
@@ -31,16 +31,24 @@ function kartan_alustus() {
       '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
   }).addTo(map);
 
-  var bounds = [
+  var default_bounds = [
     [62.239172, 25.734671],
     [62.246546, 25.750505],
     [62.243717, 25.759753],
     [62.23542, 25.742759],
   ];
 
-  map.fitBounds(bounds);
-  map.setMaxBounds(bounds);
-  map.options.minZoom = 14;
+  var max_bounds = [
+    [62.243725, 25.643903],
+    [62.286203, 25.749468],
+    [62.245303, 25.909074],
+    [62.18056, 25.745323],
+  ];
+
+  map.fitBounds(default_bounds);
+  map.setMaxBounds(max_bounds);
+  map.options.minZoom = 12;
+  map.options.maxZoom = 17;
 }
 
 function tarkista_koko() {
